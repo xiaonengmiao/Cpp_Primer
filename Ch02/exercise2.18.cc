61d031695039dd50ad15478be02e30604713c0a7
xiaonengmiao <1123913594@qq.com>
2016-09-07 10:47:49 +0800

    add ex2.18

--- Ch02/exercise2.18.cc
+++ Ch02/exercise2.18.cc
@@ -7,9 +7,11 @@ int main()
     int *pi = 0;
     int i = 42;
     std::cout << pi << std::endl;
+    // change the value of a pointer
     pi = &i;
     std::cout << pi << std::endl;
     std::cout << i << std::endl;
+    // change the value to which a pointer points
     *pi = 0;
     std::cout << pi << std::endl;
     std::cout << i << std::endl;

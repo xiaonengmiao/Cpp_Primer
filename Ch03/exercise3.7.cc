12ffed82b2028bfa984929202ceaee71c85a8130
xiaonengmiao <1123913594@qq.com>
2016-09-14 16:59:46 +0800

    add ex3.7

---
+++ Ch03/exercise3.7.cc
@@ -0,0 +1,16 @@
+// change all the characters in a string to X
+
+#include <iostream>
+// using declarations for names from the standard library
+using namespace std;
+
+int main()
+{
+    string line("Hello World!!!");
+    // string line is "Hello World"
+    for (char &c : line) {
+        c = 'X';
+    }
+    cout << line << endl;
+    return 0;
+}

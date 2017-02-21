30d69cacf5fad9368aa77a8a78c00e5b147cd43b
xiaonengmiao <1123913594@qq.com>
2017-02-21 16:08:13 +0800

    write readme

---
+++ Ch07/exercise7.2.cc
@@ -0,0 +1,28 @@
+// Add the combine and isbn members to the Sales_data class you wrote for the exercise in 2.6.2
+// (p.76)
+
+#ifndef SALES_DATA.H
+#define SALES_DATA.H
+
+#include <iostream>
+#include <string>
+
+struct Sales_data
+{
+    std::string isbn() const { return bookNo; }
+    Sales_data &combine(const Sales_data&);
+
+    std::string bookNo;
+    unsigned units_sold = 0;
+    double revenue = 0.0;
+};
+
+Sales_data &combine(const Sales_data &rhs)
+{
+    bookNo += rhs.bookNo;
+    units_sold += rhs.units_sold;
+
+    return *this;
+}
+
+#endif

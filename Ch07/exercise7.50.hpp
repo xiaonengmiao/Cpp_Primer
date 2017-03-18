95c51f66254ebad4cc7b30a6554239c8523d783e
xiaonengmiao <1123913594@qq.com>
2017-03-18 14:52:37 +0800

    add ex7.50

---
+++ Ch07/exercise7.50.hpp
@@ -0,0 +1,35 @@
+#ifndef EXERCISE_7_50_H
+#define EXERCISE_7_50_H
+
+#include <string>
+#include <iostream>
+
+struct Person {
+    friend std::istream &read(std::istream &is, Person &person);
+    friend std::ostream &print(std::ostream &os, const Person &person);
+
+public:
+    Person() = default;
+    Person(const std::string sname, const std::string saddr):name(sname), address(saddr){ }
+    explicit Person(std::istream &is){ read(is, *this); }
+
+    std::string getName() const { return name; }
+    std::string getAddress() const { return address; }
+private:
+    std::string name;
+    std::string address;
+};
+
+std::istream &read(std::istream &is, Person &person)
+{
+    is >> person.name >> person.address;
+    return is;
+}
+
+std::ostream &print(std::ostream &os, const Person &person)
+{
+    os << person.name << " " << person.address;
+    return os;
+}
+
+#endif

08316847b7482f28ced2a8a87571da07801c02b1
xiaonengmiao <1123913594@qq.com>
2017-03-18 16:29:40 +0800

    add ex7.57

---
+++ Ch07/exercise7.57.hpp
@@ -0,0 +1,22 @@
+#ifndef EXERCISE_7_57_H
+#define EXERCISE_7_57_H
+
+#include <string>
+
+class Account {
+public:
+    void calculate() { amount += amount * interestRate; }
+    static double rate() { return interestRate; }
+    static void rate(double newRate) { interestRate = newRate; }
+    
+private:
+    std::string owner;
+    double amount;
+    static double interestRate;
+    static constexpr double todayRate = 42.42;
+    static double initRate() { return todayRate; }
+};
+
+double Account::interestRate = initRate();
+
+#endif

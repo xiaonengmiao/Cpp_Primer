4fd7777d2ab7acb11f59d772a57e759841131b5d
xiaonengmiao <1123913594@qq.com>
2017-03-22 14:29:01 +0800

    add ex8.8

---
+++ Ch08/exercise8.8.cpp
@@ -0,0 +1,37 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "../Ch07/exercise7.26.hpp"
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    ifstream ifs(argv[1]);
+    ofstream ofs(argv[2], ofstream::app);
+
+    Sales_data total;
+    if (read(ifs, total))
+    {
+        Sales_data trans;
+        while (read(ifs, trans))
+        {
+            if (total.isbn() == trans.isbn())
+                total.combine(trans);
+            else
+            {
+                print(ofs, total) << std::endl;
+                total = trans;
+            }
+        }
+        print(ofs, total) << std::endl;
+    }
+    else
+    {
+        std::cerr << "No data?!" << std::endl;
+    }
+
+    return 0;
+}

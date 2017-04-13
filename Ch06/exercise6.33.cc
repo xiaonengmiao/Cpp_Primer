0123bec770715ce02eb599346104429ed7b65cf4
xiaonengmiao <1123913594@qq.com>
2017-04-13 16:18:41 +0800

    add ex6.33

---
+++ Ch06/exercise6.33.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+
+using std::vector; using std::cout; using std::endl;
+using Iter = vector<int>::const_iterator;
+
+void print(Iter first, Iter last)
+{
+    if (first != last)
+    {
+        cout << *first << " ";
+        print(++first, last);
+    }
+}
+
+int main()
+{
+    vector<int> vec{ 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    print(vec.cbegin(), vec.cend());
+    cout << endl;
+
+    return 0;
+}

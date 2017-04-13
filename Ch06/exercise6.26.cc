0f941b9710bb9ab2222c45445b6712d6470b9df1
xiaonengmiao <1123913594@qq.com>
2017-04-13 11:28:17 +0800

    add ex6.26

---
+++ Ch06/exercise6.26.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+  string str;
+  for (int i = 1; i != argc; ++i)
+    str += string(argv[i]) + " ";
+  cout << str << endl;
+
+  return 0;
+}

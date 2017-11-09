d3038d0a3a3330a277b90f00cf1e8dd66c2bf53c
Haohan Li <hlibb@connect.ust.hk>
2017-11-09 19:49:05 +0800

    add ex12.02

--- Ch12/exercise12.02.cpp
+++ Ch12/exercise12.02.cpp
@@ -1,1 +1,14 @@
 #include <iostream>
+#include "exercise12.02.hpp"
+
+int main()
+{
+  const StrBlob csb{ "hello", "world", "pezy" };
+  StrBlob sb{ "hello", "world", "Mooophy" };
+
+  std::cout << csb.front() << " " << csb.back() << std::endl;
+  sb.back() = "pezy";
+  std::cout << sb.front() << " " << sb.back() << std::endl;
+
+  return 0;
+}
--- Ch12/exercise12.02.hpp
+++ Ch12/exercise12.02.hpp
@@ -1,4 +1,9 @@
 #include <iostream>
+#include <vector>
+#include <string>
+#include <memory>
+#include <initializer_list>
+#include <exception>
 
 class StrBlob 
 {
@@ -7,8 +12,8 @@ class StrBlob
     StrBlob();
     StrBlob(std::initializer_list<std::string> il);
 
-    std::string &front();
-    std::string &back();
+    std::string &front() const;
+    std::string &back() const;
 
   private:
     std::shared_ptr<std::vector<std::string>> data;
@@ -16,9 +21,9 @@ class StrBlob
 
 };
 
-StrBlob::StrBlob(): data(make_shared<std::vector<std::string>>()) { }
+StrBlob::StrBlob(): data(std::make_shared<std::vector<std::string>>()) { }
 
-StrBlob::StrBlob(std::initializer_list<std::string> il): data(make_shared<std::vector<std::string>>(il) { }
+StrBlob::StrBlob(std::initializer_list<std::string> il): data(std::make_shared<std::vector<std::string>>(il)) { }
 
 std::string& StrBlob::front() const 
 {

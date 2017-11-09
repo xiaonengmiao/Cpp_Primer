75f54c2c0c39c474b4071f4241bd7a822957b8e7
Haohan Li <hlibb@connect.ust.hk>
2017-11-09 16:49:51 +0800

    write ex12.02

--- Ch12/exercise12.02.hpp
+++ Ch12/exercise12.02.hpp
@@ -4,5 +4,28 @@ class StrBlob
 {
   public:
     typedef std::vector<std::string>::size_type size_type;
+    StrBlob();
+    StrBlob(std::initializer_list<std::string> il);
 
+    std::string &front();
+    std::string &back();
+
+  private:
+    std::shared_ptr<std::vector<std::string>> data;
+
+
+};
+
+StrBlob::StrBlob(): data(make_shared<std::vector<std::string>>()) { }
+
+StrBlob::StrBlob(std::initializer_list<std::string> il): data(make_shared<std::vector<std::string>>(il) { }
+
+std::string& StrBlob::front() const 
+{
+    return data->front();
+}
+
+std::string& StrBlob::back() const
+{
+    return data->back();
 }

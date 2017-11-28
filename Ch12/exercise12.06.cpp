9d9ce8894cdeaf0d0032ed1c52efb91fd3ee6a46
Haohan Li <hlibb@connect.ust.hk>
2017-11-28 15:42:05 +0800

    add ex12.06

---
+++ Ch12/exercise12.06.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+
+std::vector<int> *dynamiclly_allocate()
+{
+  return new std::vector<int>{ };
+}
+
+std::vector<int> *initialize(std::vector<int> *p)
+{
+  int i;
+  while(std::cin >> i)
+  {
+    p->push_back(i);
+  }
+
+  return p;
+}
+
+auto print(std::vector<int> *p) -> std::ostream &
+{
+  for(auto i : *p)
+  {
+    std::cout << i << " ";
+  }
+
+  return std::cout;
+}
+
+int main()
+{
+  auto vec = initialize(dynamiclly_allocate());
+  print(vec) << std::endl;
+
+  return 0;
+}

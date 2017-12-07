899697f1d71afc9d760c5cbdeb00f8246c256382
Haohan Li <hlibb@connect.ust.hk>
2017-12-07 20:16:03 +0800

    add ex15.18/29

---
+++ Ch15/ex15.28.29/quote.cpp
@@ -0,0 +1,8 @@
+#include "quote.hpp"
+
+void Quote::debug() const
+{
+    std::cout //<< "data members of this class:\n"
+              << "bookNo= " <<this->bookNo << " "
+              << "price= " <<this->price<< " \n";
+}
